aee1dbf9e7d1b027452a050c7ba4d059d13f470a
LLIaJIyHuLLIKa <vlad.tokan96@mail.ru>
2020-06-03 22:35:10 +0300

    Updates

---
+++ precache-manifest.f16e7974b250e79bc84344b0a0995681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286781325fc33f8766b966a4659be0c1",
+    "url": "/nrgsoft-test/index.html"
+  },
+  {
+    "revision": "80a4420c9aa0f5c9544f",
+    "url": "/nrgsoft-test/static/css/main.ded744d2.chunk.css"
+  },
+  {
+    "revision": "9237ab13cd2667f5af48",
+    "url": "/nrgsoft-test/static/js/2.f1dbc609.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nrgsoft-test/static/js/2.f1dbc609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a4420c9aa0f5c9544f",
+    "url": "/nrgsoft-test/static/js/main.75c30f23.chunk.js"
+  },
+  {
+    "revision": "93195fb81f946bba28e2",
+    "url": "/nrgsoft-test/static/js/runtime-main.9a47bde6.js"
+  }
+]);
